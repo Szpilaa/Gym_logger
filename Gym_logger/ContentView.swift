fee8804d5c6a9500dfed45db81259e2d0ec3efb7
Szpilaa <k.szpila01@gmail.com>
2024-09-08 14:43:49 +0200

    Initial Commit

---
+++ Gym_logger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gym_logger
+//
+//  Created by Kacper Szpila on 08/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gym_logger/Gym_loggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_loggerApp.swift
+//  Gym_logger
+//
+//  Created by Kacper Szpila on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gym_loggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
